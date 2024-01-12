338e74f20570cf9cc132db93eba44f8c651b81bf
adm.krotenko <aleksey.s.krotenko@gmail.com>
2024-01-12 11:11:30 +0500

    add footer nav - mock data

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,6 @@
 <script setup>
 import TheHeader from './components/TheHeader.vue'
+import { ClockIcon, ListBulletIcon, ChartBarIcon } from '@heroicons/vue/24/outline'
 </script>
 
 <template>
@@ -15,4 +16,36 @@ import TheHeader from './components/TheHeader.vue'
     excepturi eum, reiciendis adipisci beatae omnis. Doloremque quia et exercitationem ratione eius
     dolorem. Assumenda dicta corrupti eligendi aliquam, maxime iusto expedita suscipit! Eos!
   </main>
+
+  <nav class="sticky bottom-0 z-10 bg-white">
+    <ul class="flex items-center justify-around border-t">
+      <li class="flex-1">
+        <a
+          href="#timeline"
+          class="flex flex-col items-center p-2 text-xs capitalize hover:bg-gray-100"
+        >
+          <ClockIcon class="h-6 w-6" />
+          Timeline
+        </a>
+      </li>
+      <li class="flex-1">
+        <a
+          href="#activities"
+          class="flex flex-col items-center p-2 text-xs capitalize hover:bg-gray-100"
+        >
+          <ListBulletIcon class="h-6 w-6" />
+          Activities
+        </a>
+      </li>
+      <li class="flex-1">
+        <a
+          href="#progress"
+          class="flex flex-col items-center p-2 text-xs capitalize hover:bg-gray-100"
+        >
+          <ChartBarIcon class="h-6 w-6" />
+          Progress
+        </a>
+      </li>
+    </ul>
+  </nav>
 </template>

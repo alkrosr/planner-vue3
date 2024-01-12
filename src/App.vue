7b1276f7afe919763a7507ac112333c8c68796ce
adm.krotenko <aleksey.s.krotenko@gmail.com>
2024-01-12 10:42:39 +0500

    add header - mock data

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,18 @@
-<template>
-  <header class="sticky top-0 z-20 flex items-center justify-between border-b bg-white p-3">
-    test
-  </header>
-</template>
+<script setup>
+import TheHeader from './components/TheHeader.vue'
+</script>
 
+<template>
+  <TheHeader />
+
+  <main class="flex flex-grow flex-col">
+    Lorem ipsum dolor sit amet consectetur adipisicing elit. Optio modi nesciunt, odit, eius quaerat
+    veniam sint voluptate maxime non totam similique possimus laboriosam! Aperiam aliquam quae
+    tempore unde iure fuga vero magni doloremque. Architecto porro ducimus reiciendis quas assumenda
+    voluptates quibusdam adipisci nisi, quos tempore dolor sed ab iste aliquid soluta consequatur
+    hic? Quaerat, eaque itaque. Ullam illo repellat cumque similique reprehenderit amet consequatur
+    labore nemo veritatis eius! Quae sit culpa provident ipsam voluptas soluta quaerat blanditiis
+    excepturi eum, reiciendis adipisci beatae omnis. Doloremque quia et exercitationem ratione eius
+    dolorem. Assumenda dicta corrupti eligendi aliquam, maxime iusto expedita suscipit! Eos!
+  </main>
+</template>
---
+++ src/components/TheHeader.vue
@@ -0,0 +1,20 @@
+<script setup>
+import { CheckCircleIcon } from '@heroicons/vue/24/solid'
+</script>
+
+<template>
+  <header class="sticky top-0 z-20 flex items-center justify-between border-b bg-white p-3">
+    <a href="#">
+      <div>MY PLANNER</div>
+    </a>
+    <a href="#" class="text-sm">
+      <div v-if="true" class="flex items-center gap-1">
+        <div>Progress: <span class="font-mono">33%</span></div>
+        <div class="h-3 w-3 rounded-full bg-red-500"></div>
+      </div>
+      <div v-else class="flex items-center gap-1">
+        Day complete! <CheckCircleIcon class="h-7 text-green-500" />
+      </div>
+    </a>
+  </header>
+</template>
